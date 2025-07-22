3abe8080157c6ec848cbb901403d10f92efa507c
davebhakti <bhaktidave2206@gmail.com>
2025-07-21 23:25:35 -0700

    Changed equality to == instead of === as it means the datatype has to be same, which is something we do not want

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ async function checkVisisted() {
 async function getCurrentUser() {
   const result = await db.query("SELECT * FROM users");
   users = result.rows;
-  return users.find((user) => user.id === currentUserId);
+  return users.find((user) => user.id == currentUserId);
 }
 
 app.get("/", async (req, res) => {
